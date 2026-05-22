c75e3d87a1d122a33de4f80399ef9adc0fa2ae8b
Elias Casa <eliasaaronalfaro@gmail.com>
2026-05-22 18:37:08 -0400

    creacion frontend-deploy

--- src/componentes/CrudAdmin/FormDespacho.jsx
+++ src/componentes/CrudAdmin/FormDespacho.jsx
@@ -2,6 +2,7 @@ import { useForm } from "react-hook-form";
 import Swal from "sweetalert2";
 import axios from "axios";
 
+const API_BASE_URL = import.meta.env.VITE_API_URL || '';
 export const FormDespacho = ({ venta, onClose }) => {
   const { register, handleSubmit } = useForm();
 
@@ -25,7 +26,7 @@ export const FormDespacho = ({ venta, onClose }) => {
 
     try {
       await axios.put(
-        `http://192.168.30/api/v1/ventas/${venta.idVenta}`,
+        `${API_BASE_URL}/api/v1/ventas/${venta.idVenta}`,
         jsonDataSales,
         {
           headers:{
@@ -34,7 +35,7 @@ export const FormDespacho = ({ venta, onClose }) => {
       }
         }
       );
-      await axios.post("http://192.168.320/api/v1/despachos", jsonData, {
+      await axios.post("${API_BASE_URL}/api/v1/despachos", jsonData, {
         headers:{
           'Content-Type': 'application/json',
           'Accept': 'application/json'
